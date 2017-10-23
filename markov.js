c7f90fedb809889c8071be1c9f2b29e5a2086b9c
THEJOSHBOX2000\Josh <joshlorenzen49@gmail.com>
2017-10-22 20:28:35 -0700

    Added constant NGRAM to determine size of word keys

--- markov.js
+++ markov.js
@@ -5,21 +5,22 @@ var words = ["sodium", "magnesium", "potassium", "helium", "lithium", "calcium",
 	"plutonium", "curium", "fermium", "nobelium", "erbium", "berkelium", "indium", "iridium", "lutetium", "meitnerium", "neobymium", "niobium", "osmium"];
 var starts = [];
 
-var maxLength = 20;
+var MAX_LENGTH = 20;
+var NGRAM = 2;
 
 function learnWord(chain, word)
 {
-	starts.push(word.slice(0, 2));
-	for(var i = 0; i < word.length - 2; i++)
+	starts.push(word.slice(0, NGRAM));
+	for(var i = 0; i < word.length - NGRAM; i++)
 	{
-		var key = word.slice(i, i + 2);
+		var key = word.slice(i, i + NGRAM);
 		if(chain[key] == null)
 		{
-			chain[key] = [word[i + 2]];
+			chain[key] = [word[i + NGRAM]];
 		}
 		else
 		{
-			chain[key].push(word[i + 2]);
+			chain[key].push(word[i + NGRAM]);
 		}
 	}
 	console.log(chain);
@@ -30,12 +31,12 @@ function buildWord(chain)
 	var word = starts[Math.floor(Math.random() * starts.length)];
 	while(true)
 	{
-		nextArray = chain[word.slice(-2)]
+		nextArray = chain[word.slice(-NGRAM)]
 		if(nextArray == undefined)
 			return word;
 		next = nextArray[Math.floor(Math.random() * nextArray.length)]
 		word += next;
-		if(word.length > maxLength)
+		if(word.length > MAX_LENGTH)
 			return word;
 		console.log(word);
 	}
